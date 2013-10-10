19e0eb6230df3782f9fe182e1b516b38e4a8c383
Jason Reilly <jdr0887@renci.org>
2013-10-10 15:25:30 -0400

    flushing out impl

--- mapseq-tasks/src/main/java/edu/unc/mapseq/tasks/WeeklyReportTask.java
+++ mapseq-tasks/src/main/java/edu/unc/mapseq/tasks/WeeklyReportTask.java
@@ -1,9 +1,14 @@
 package edu.unc.mapseq.tasks;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.mail.EmailAttachment;
 import org.apache.commons.mail.EmailException;
@@ -11,10 +16,22 @@ import org.apache.commons.mail.MultiPartEmail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import edu.unc.mapseq.dao.AccountDAO;
 import edu.unc.mapseq.dao.MaPSeqDAOBean;
 import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.WorkflowRunDAO;
 import edu.unc.mapseq.dao.model.Account;
+import edu.unc.mapseq.dao.model.Job;
+import edu.unc.mapseq.dao.model.Workflow;
+import edu.unc.mapseq.dao.model.WorkflowRun;
 import edu.unc.mapseq.reports.ReportFactory;
 
 public class WeeklyReportTask implements Runnable {
@@ -37,17 +54,71 @@ public class WeeklyReportTask implements Runnable {
         c.add(Calendar.WEEK_OF_YEAR, -1);
         Date startDate = c.getTime();
 
+        Document document = new Document(PageSize.LETTER.rotate());
+
         try {
             File pdfFile = File.createTempFile("weeklyReport-", ".pdf");
 
-            String username = System.getProperty("user.name");
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(pdfFile));
+            writer.setCompressionLevel(0);
+
+            document.open();
+            document.setMargins(5, 5, 5, 5);
+
+            // String username = System.getProperty("user.name");
+            String username = "rc_renci.svc";
             AccountDAO accountDAO = maPSeqDAOBean.getAccountDAO();
             Account account = accountDAO.findByName(username);
 
-            File workflowRunReportFile = ReportFactory.createWorkflowRunReport(maPSeqDAOBean, account, startDate,
-                    endDate);
+            WorkflowRunDAO workflowRunDAO = maPSeqDAOBean.getWorkflowRunDAO();
+            List<WorkflowRun> workflowRunList = workflowRunDAO.findByCreatorAndCreationDateRange(account.getId(),
+                    startDate, endDate);
 
-            
+            document.add(new Paragraph());
+            File workflowRunReportFile = ReportFactory.createWorkflowRunReport(workflowRunList, account, startDate,
+                    endDate);
+            Image img = Image.getInstance(workflowRunReportFile.getAbsolutePath());
+            img.setAlignment(Element.ALIGN_CENTER);
+            img.scalePercent(80, 80);
+            document.add(img);
+
+            workflowRunReportFile.delete();
+
+            Set<Workflow> workflowSet = new HashSet<Workflow>();
+            for (WorkflowRun workflowRun : workflowRunList) {
+                workflowSet.add(workflowRun.getWorkflow());
+            }
+
+            Set<Workflow> synchronizedWorkflowSet = Collections.synchronizedSet(workflowSet);
+            for (Workflow workflow : synchronizedWorkflowSet) {
+
+                document.newPage();
+
+                List<Job> jobList = maPSeqDAOBean.getJobDAO().findByCreatorAndWorkflowIdAndCreationDateRange(
+                        account.getId(), workflow.getId(), startDate, endDate);
+
+                document.add(new Paragraph());
+                File workflowJobsPerClusterReportFile = ReportFactory.createWorkflowJobsPerClusterReport(jobList,
+                        account, workflow, startDate, endDate);
+                img = Image.getInstance(workflowJobsPerClusterReportFile.getAbsolutePath());
+                img.setAlignment(Element.ALIGN_CENTER);
+                img.scalePercent(60, 60);
+                document.add(img);
+
+                document.add(new Paragraph());
+                File workflowJobsReportFile = ReportFactory.createWorkflowJobsReport(jobList, account, workflow,
+                        startDate, endDate);
+                img = Image.getInstance(workflowJobsReportFile.getAbsolutePath());
+                img.scalePercent(80, 80);
+                img.setAlignment(Element.ALIGN_CENTER);
+                document.add(img);
+
+                workflowJobsPerClusterReportFile.delete();
+                workflowJobsReportFile.delete();
+            }
+
+            document.close();
+
             EmailAttachment attachment = new EmailAttachment();
             attachment.setPath(pdfFile.getAbsolutePath());
             attachment.setDisposition(EmailAttachment.ATTACHMENT);
@@ -63,11 +134,9 @@ public class WeeklyReportTask implements Runnable {
             email.attach(attachment);
 
             email.send();
-        } catch (MaPSeqDAOException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (EmailException e) {
+
+            pdfFile.delete();
+        } catch (IOException | DocumentException | MaPSeqDAOException | EmailException e) {
             e.printStackTrace();
         }
 
