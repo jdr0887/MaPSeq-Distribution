71a6367710b2ea1738ec44f7d518530d63d9cb48
Jason Reilly <jdr0887@renci.org>
2014-08-28 11:53:06 -0400

    misc

--- mapseq-commands/src/test/java/edu/unc/mapseq/commands/Scratch.java
+++ mapseq-commands/src/test/java/edu/unc/mapseq/commands/Scratch.java
@@ -1,24 +1,15 @@
 package edu.unc.mapseq.commands;
 
-import java.text.ParseException;
-import java.util.Date;
-
-import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 
 public class Scratch {
 
     @Test
     public void test() {
-
-        try {
-            String s = "06/30/14 12:38:16 All jobs Completed!";
-            String[] lineSplit = s.split(" ");
-            Date endDate = DateUtils.parseDate(String.format("%s %s", lineSplit[0], lineSplit[1]),
-                    new String[] { "MM/dd/yy HH:mm:ss" });
-            System.out.println(endDate.toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        String flowcellName = "121119_UNC12-SN629_0239_BC11DEACXX";
+        String path = "/proj/seq/mapseq/RENCI/121119_UNC12-SN629_0239_BC11DEACXX/NIDAUCSF/L002_AGTTCC";
+        String pathToFlowcell = path.substring(0, path.indexOf(flowcellName) - 1);
+        System.out.println(pathToFlowcell);
     }
+
 }
