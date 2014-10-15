7d8da26f1b2bb02669eb06244aea04d96cb07f76
Jason Reilly <jdr0887@gmail.com>
2014-10-15 16:17:19 -0400

    fixing argument & option packages

--- mapseq-commands/src/main/java/edu/unc/mapseq/commands/reports/WeeklyReportAction.java
+++ mapseq-commands/src/main/java/edu/unc/mapseq/commands/reports/WeeklyReportAction.java
@@ -2,8 +2,8 @@ package edu.unc.mapseq.commands.reports;
 
 import java.util.concurrent.Executors;
 
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.AbstractAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
