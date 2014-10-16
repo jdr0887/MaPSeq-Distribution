82d1878222e81188d068ecc96ffb674be1fbc8fb
Jason Reilly <jdr0887@gmail.com>
2014-10-16 16:06:12 -0400

    initial commit

---
+++ mapseq-dao/mapseq-dao-soap/src/test/java/edu/unc/mapseq/dao/ws/AttributeDAOTest.java
@@ -0,0 +1,27 @@
+package edu.unc.mapseq.dao.ws;
+
+import org.junit.Test;
+
+import edu.unc.mapseq.dao.AttributeDAO;
+import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.model.Attribute;
+
+public class AttributeDAOTest {
+
+    @Test
+    public void save() {
+
+        WSDAOManager daoMgr = WSDAOManager.getInstance("edu/unc/mapseq/dao/ws/mapseq-dao-beans-test.xml");
+
+        final AttributeDAO attributeDAO = daoMgr.getMaPSeqDAOBean().getAttributeDAO();
+
+        Attribute attribute = new Attribute("asdf", "asdf");
+        try {
+            Long id = attributeDAO.save(attribute);
+            System.out.println(id);
+        } catch (MaPSeqDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
