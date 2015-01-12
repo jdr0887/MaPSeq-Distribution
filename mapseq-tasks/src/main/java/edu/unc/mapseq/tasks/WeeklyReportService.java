6047421734d0d206cf698914f699232abcc47be5
Jason Reilly <jdr0887@gmail.com>
2015-01-12 12:28:07 -0500

    adding enabled hook

--- mapseq-tasks/src/main/java/edu/unc/mapseq/tasks/WeeklyReportService.java
+++ mapseq-tasks/src/main/java/edu/unc/mapseq/tasks/WeeklyReportService.java
@@ -16,22 +16,27 @@ public class WeeklyReportService {
 
     private WeeklyReportTask task;
 
+    private Boolean enabled = Boolean.TRUE;
+
     public WeeklyReportService() {
         super();
         this.scheduler = Executors.newSingleThreadScheduledExecutor();
     }
 
     public void start() throws Exception {
-        logger.info("ENTERING start()");
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
-        c.set(Calendar.HOUR_OF_DAY, 1);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-        c.add(Calendar.DATE, 7);
-        long delay = c.getTimeInMillis() - System.currentTimeMillis();
-        long period = 1000 * 60 * 60 * 24 * 7;
-        scheduler.scheduleAtFixedRate(task, delay, period, TimeUnit.MILLISECONDS);
+        logger.debug("ENTERING start()");
+        logger.info("enabled: {}", enabled.toString());
+        if (enabled) {
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+            c.set(Calendar.HOUR_OF_DAY, 1);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            c.add(Calendar.DATE, 7);
+            long delay = c.getTimeInMillis() - System.currentTimeMillis();
+            long period = 1000 * 60 * 60 * 24 * 7;
+            scheduler.scheduleAtFixedRate(task, delay, period, TimeUnit.MILLISECONDS);
+        }
     }
 
     public void stop() throws Exception {
@@ -45,6 +50,14 @@ public class WeeklyReportService {
 
     public void setTask(WeeklyReportTask task) {
         this.task = task;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
     }
 
 }
