8fcbb20242e93870df131ecaa2a52441bf8cd215
Jason Reilly <jdr0887@gmail.com>
2016-02-23 09:43:39 -0500

    misc

--- mapseq-modules/src/test/java/edu/unc/mapseq/module/Scratch.java
+++ mapseq-modules/src/test/java/edu/unc/mapseq/module/Scratch.java
@@ -21,10 +21,12 @@ public class Scratch {
     @Test
     public void scratch() {
 
-        DecimalFormat df = new DecimalFormat();
-        df.setMaximumFractionDigits(6);
-        System.out.println(df.format(Double.valueOf("-0.0001")));
-        System.out.println(String.format("%f", Double.valueOf("-0.0001")));
+        System.out.println(String.format("%2$s%1$s%3$s%1$s%4$s", File.separator, "asdf", "generate-sources", "modules"));
+
+        // DecimalFormat df = new DecimalFormat();
+        // df.setMaximumFractionDigits(6);
+        // System.out.println(df.format(Double.valueOf("-0.0001")));
+        // System.out.println(String.format("%f", Double.valueOf("-0.0001")));
     }
 
     @Test
