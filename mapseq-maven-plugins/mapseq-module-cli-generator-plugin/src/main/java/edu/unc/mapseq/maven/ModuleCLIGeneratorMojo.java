91ee45d05c85daa913dd0ae859edf4764de9aac1
Jason Reilly <jdr0887@gmail.com>
2016-02-23 09:42:36 -0500

    maven api/core/plugins have changed, using plugin annotations

--- mapseq-maven-plugins/mapseq-module-cli-generator-plugin/src/main/java/edu/unc/mapseq/maven/ModuleCLIGeneratorMojo.java
+++ mapseq-maven-plugins/mapseq-module-cli-generator-plugin/src/main/java/edu/unc/mapseq/maven/ModuleCLIGeneratorMojo.java
@@ -4,126 +4,104 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.apache.maven.artifact.Artifact;
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.classworlds.ClassRealm;
-import org.codehaus.classworlds.ClassWorld;
-import org.codehaus.classworlds.DuplicateRealmException;
 
 import edu.unc.mapseq.generator.ModuleCLIGenerator;
 import edu.unc.mapseq.module.annotations.Application;
 import edu.unc.mapseq.module.annotations.Ignore;
 
-/**
- * @goal generate
- * @phase generate-sources
- * @requiresProject true
- * @description
- * 
- * @author jdr0887
- * 
- */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresProject = true, requiresDependencyResolution = ResolutionScope.COMPILE)
 public class ModuleCLIGeneratorMojo extends AbstractMojo {
 
-    /**
-     * @parameter default-value="${project}"
-     * @required
-     * @readonly
-     */
+    @Component
     private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-        ClassWorld world = new ClassWorld();
-        ClassRealm jpaRealm;
-
         try {
-            jpaRealm = world.newRealm("maven.plugin.mapseq", Thread.currentThread().getContextClassLoader());
-        } catch (DuplicateRealmException e) {
-            throw new MojoExecutionException("Problem while creating new ClassRealm", e);
-        }
 
-        List<File> pathList = new ArrayList<File>();
-        try {
-            Iterator<?> itor = project.getCompileClasspathElements().iterator();
-            while (itor.hasNext()) {
-                File pathElem = new File((String) itor.next());
-                pathList.add(pathElem);
-                URL url = pathElem.toURI().toURL();
-                jpaRealm.addConstituent(url);
+            List<URL> urls = new ArrayList<URL>();
+            for (String fileName : project.getCompileClasspathElements()) {
+                File pathElem = new File(fileName);
+                urls.add(pathElem.toURI().toURL());
             }
 
-            Set<?> artifacts = project.getDependencyArtifacts();
-            for (Iterator<?> artifactIterator = artifacts.iterator(); artifactIterator.hasNext();) {
-                Artifact artifact = (Artifact) artifactIterator.next();
-                if (!"test".equals(artifact.getScope())) {
-                    File file = artifact.getFile();
-                    URL url = file.toURI().toURL();
-                    jpaRealm.addConstituent(url);
+            ClassLoader cl = new URLClassLoader(urls.toArray(new URL[urls.size()]), getClass().getClassLoader());
+
+            Thread.currentThread().setContextClassLoader(cl);
+
+            List<Class<?>> classList = new ArrayList<Class<?>>();
+
+            String targetClassesPath = null;
+            for (URL url : urls) {
+                if (url.getFile().contains("target/classes")) {
+                    targetClassesPath = url.getFile();
+                    break;
                 }
             }
+
+            if (StringUtils.isNotEmpty(targetClassesPath)) {
+                Collection<File> files = FileUtils.listFiles(new File(targetClassesPath),
+                        FileFilterUtils.asFileFilter(new FilenameFilter() {
+                            @Override
+                            public boolean accept(File dir, String name) {
+                                if (name.endsWith(".class") && name.indexOf("$") == -1 && !name.contains("CLI")) {
+                                    return true;
+                                }
+                                return false;
+                            }
+                        }), TrueFileFilter.INSTANCE);
+                for (File f : files) {
+                    try {
+                        int idx = f.getAbsolutePath().indexOf("target/classes/");
+                        String fullClassName = f.getAbsolutePath().replace(".class", "");
+                        fullClassName = fullClassName.substring(idx + 15, fullClassName.length());
+                        fullClassName = fullClassName.replace("/", ".");
+                        classList.add(Class.forName(fullClassName, false, cl));
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            List<Class<?>> filteredClassList = new ArrayList<Class<?>>();
+            classList.forEach(a -> {
+                if (!a.isAnnotationPresent(Ignore.class) && a.isAnnotationPresent(Application.class)) {
+                    filteredClassList.add(a);
+                }
+            });
+
+            // filteredClassList.forEach(a -> getLog().info(a.getName()));
+
+            String pkg = "edu.unc.mapseq.module";
+            ModuleCLIGenerator generator = new ModuleCLIGenerator(filteredClassList, pkg,
+                    String.format("%2$s%1$s%3$s%1$s%4$s", File.separator, project.getBuild().getDirectory(),
+                            "generated-sources", "modules"));
+            generator.run();
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (DependencyResolutionRequiredException e) {
             e.printStackTrace();
         }
-        Thread.currentThread().setContextClassLoader(jpaRealm.getClassLoader());
-
-        // set the new ClassLoader as default for this Thread
-        List<Class<?>> classList = new ArrayList<Class<?>>();
-
-        for (File path : pathList) {
-
-            Collection<File> files = org.apache.commons.io.FileUtils.listFiles(path,
-                    FileFilterUtils.asFileFilter(new FilenameFilter() {
-                        @Override
-                        public boolean accept(File dir, String name) {
-                            if (name.endsWith(".class") && name.indexOf("$") == -1 && !name.contains("CLI")) {
-                                return true;
-                            }
-                            return false;
-                        }
-                    }), TrueFileFilter.INSTANCE);
-            for (File f : files) {
-                int idx = f.getAbsolutePath().indexOf("target/classes/");
-                String fullClassName = f.getAbsolutePath().replace(".class", "");
-                fullClassName = fullClassName.substring(idx + 15, fullClassName.length());
-                fullClassName = fullClassName.replace("/", ".");
-                try {
-                    classList.add(Class.forName(fullClassName, false, jpaRealm.getClassLoader()));
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-
-        List<Class<?>> filteredClassList = new ArrayList<Class<?>>();
-        for (Class<?> c : classList) {
-            if (c.isAnnotationPresent(Ignore.class) || !c.isAnnotationPresent(Application.class)) {
-                continue;
-            }
-
-            filteredClassList.add(c);
-        }
-
-        String pkg = "edu.unc.mapseq.module";
-        ModuleCLIGenerator generator = new ModuleCLIGenerator(filteredClassList, pkg, project.getBuild().getDirectory()
-                + File.separator + "generated-sources" + File.separator + "modules");
-        generator.run();
 
     }
 
