15919db85c41c54dabe8938619be088d32fc8172
Jason Reilly <jdr0887@gmail.com>
2016-02-29 09:27:01 -0500

    moving helper function to new util

--- mapseq-workflow-impl/src/main/java/edu/unc/mapseq/workflow/impl/AbstractSampleWorkflow.java
+++ mapseq-workflow-impl/src/main/java/edu/unc/mapseq/workflow/impl/AbstractSampleWorkflow.java
@@ -1,14 +1,10 @@
 package edu.unc.mapseq.workflow.impl;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -21,7 +17,6 @@ import edu.unc.mapseq.dao.MaPSeqDAOException;
 import edu.unc.mapseq.dao.SampleDAO;
 import edu.unc.mapseq.dao.model.FileData;
 import edu.unc.mapseq.dao.model.Flowcell;
-import edu.unc.mapseq.dao.model.Job;
 import edu.unc.mapseq.dao.model.MimeType;
 import edu.unc.mapseq.dao.model.Sample;
 import edu.unc.mapseq.dao.model.Study;
@@ -132,94 +127,6 @@ public abstract class AbstractSampleWorkflow extends AbstractWorkflow {
         }
 
         return sampleSet;
-    }
-
-    public String getRootFastqName(String name) {
-        logger.debug("ENTERING getRootFastqName(String)");
-
-        if (name.endsWith(".fastq.gz")) {
-            return StringUtils.removeEnd(name, ".fastq.gz");
-        }
-        if (name.endsWith(".fastq")) {
-            return StringUtils.removeEnd(name, ".fastq");
-        }
-        return name;
-    }
-
-    public List<File> getReadPairList(Sample sample) {
-        logger.debug("ENTERING getReadPairList(Set<FileData>, String, Integer)");
-
-        List<File> readPairList = new ArrayList<File>();
-
-        Set<FileData> fileDataSet = sample.getFileDatas();
-        if (fileDataSet == null) {
-            logger.warn("fileDataSet was null...returning empty readPairList");
-            return readPairList;
-        }
-
-        logger.info("fileDataSet.size() = {}", fileDataSet.size());
-
-        for (FileData fileData : fileDataSet) {
-            MimeType mimeType = fileData.getMimeType();
-            if (mimeType != null && mimeType.equals(MimeType.FASTQ)) {
-                Pattern patternR1 = Pattern.compile(String.format("^%s.*_L00%d_R1\\.fastq\\.gz$",
-                        sample.getFlowcell().getName(), sample.getLaneIndex()));
-                Matcher matcherR1 = patternR1.matcher(fileData.getName());
-                File file = new File(fileData.getPath(), fileData.getName());
-                if (matcherR1.matches()) {
-                    logger.debug("found file: {}", file.getAbsolutePath());
-                    readPairList.add(file);
-                }
-
-                Pattern patternR2 = Pattern.compile(String.format("^%s.*_L00%d_R2\\.fastq\\.gz$",
-                        sample.getFlowcell().getName(), sample.getLaneIndex()));
-                Matcher matcherR2 = patternR2.matcher(fileData.getName());
-                if (matcherR2.matches()) {
-                    logger.debug("found file: {}", file.getAbsolutePath());
-                    readPairList.add(file);
-                }
-            }
-        }
-        readPairList.sort((a, b) -> a.getName().compareTo(b.getName()));
-
-        return readPairList;
-    }
-
-    public File findFileByJobAndMimeTypeAndWorkflowId(Set<FileData> fileDataSet, Class<?> clazz, MimeType mimeType,
-            Long workflowId) {
-        logger.debug("ENTERING findFileByJobAndMimeTypeAndWorkflowId(Set<FileData>, Class<?>, MimeType, Long)");
-
-        File ret = null;
-
-        if (fileDataSet == null) {
-            logger.warn("fileDataSet was null...returning empty file list");
-            return ret;
-        }
-
-        logger.info("fileDataSet.size() = {}", fileDataSet.size());
-
-        for (FileData fileData : fileDataSet) {
-            if (fileData.getMimeType().equals(mimeType)) {
-                List<Job> jobList = null;
-                try {
-                    jobList = getWorkflowBeanService().getMaPSeqDAOBeanService().getJobDAO()
-                            .findByFileDataIdAndWorkflowId(fileData.getId(), clazz.getName(), workflowId);
-                } catch (MaPSeqDAOException e) {
-                    e.printStackTrace();
-                }
-                if (jobList != null && jobList.size() > 0) {
-                    for (Job job : jobList) {
-                        if (job.getName().contains(clazz.getSimpleName())) {
-                            logger.debug("using FileData: {}", fileData.toString());
-                            logger.debug("from Job: {}", job.toString());
-                            ret = new File(fileData.getPath(), fileData.getName());
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return ret;
     }
 
     public File findFileByMimeTypeAndSuffix(Set<FileData> fileDataSet, MimeType mimeType, String suffix) {
