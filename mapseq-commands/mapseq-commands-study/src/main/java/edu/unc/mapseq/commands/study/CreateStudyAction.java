677caa3631385ff12d8a01e2d39c5b81feb1083f
Jason Reilly <jdr0887@gmail.com>
2016-03-08 09:57:14 -0500

    using registered daos when possible

--- mapseq-commands/mapseq-commands-study/src/main/java/edu/unc/mapseq/commands/study/CreateStudyAction.java
+++ mapseq-commands/mapseq-commands-study/src/main/java/edu/unc/mapseq/commands/study/CreateStudyAction.java
@@ -6,8 +6,8 @@ import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 
-import edu.unc.mapseq.dao.MaPSeqDAOBeanService;
 import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.StudyDAO;
 import edu.unc.mapseq.dao.model.Study;
 
 @Command(scope = "mapseq", name = "create-study", description = "Create Study")
@@ -15,7 +15,7 @@ import edu.unc.mapseq.dao.model.Study;
 public class CreateStudyAction implements Action {
 
     @Reference
-    private MaPSeqDAOBeanService maPSeqDAOBeanService;
+    private StudyDAO studyDAO;
 
     @Argument(index = 0, name = "name", description = "Name", required = true, multiValued = false)
     private String name;
@@ -28,7 +28,7 @@ public class CreateStudyAction implements Action {
     public Object execute() {
         try {
             Study study = new Study(name);
-            study.setId(maPSeqDAOBeanService.getStudyDAO().save(study));
+            study.setId(studyDAO.save(study));
             System.out.println(study.toString());
         } catch (MaPSeqDAOException e1) {
             e1.printStackTrace();
--- mapseq-commands/mapseq-commands-study/src/main/java/edu/unc/mapseq/commands/study/ListStudiesAction.java
+++ mapseq-commands/mapseq-commands-study/src/main/java/edu/unc/mapseq/commands/study/ListStudiesAction.java
@@ -1,12 +1,12 @@
 package edu.unc.mapseq.commands.study;
 
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
@@ -14,7 +14,7 @@ import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.unc.mapseq.dao.MaPSeqDAOBeanService;
+import edu.unc.mapseq.dao.MaPSeqDAOException;
 import edu.unc.mapseq.dao.StudyDAO;
 import edu.unc.mapseq.dao.model.Study;
 
@@ -22,10 +22,10 @@ import edu.unc.mapseq.dao.model.Study;
 @Service
 public class ListStudiesAction implements Action {
 
-    private final Logger logger = LoggerFactory.getLogger(ListStudiesAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(ListStudiesAction.class);
 
     @Reference
-    private MaPSeqDAOBeanService maPSeqDAOBeanService;
+    private StudyDAO studyDAO;
 
     public ListStudiesAction() {
         super();
@@ -35,33 +35,31 @@ public class ListStudiesAction implements Action {
     public Object execute() {
         logger.debug("ENTERING doExecute()");
 
-        List<Study> studyList = new ArrayList<Study>();
-        StudyDAO studyDAO = maPSeqDAOBeanService.getStudyDAO();
-
         try {
-            studyList.addAll(studyDAO.findAll());
-        } catch (Exception e) {
-        }
+            List<Study> studyList = studyDAO.findAll();
 
-        if (studyList.size() > 0) {
-            StringBuilder sb = new StringBuilder();
-            String format = "%1$-12s %2$-20s %3$-40s%n";
-            Formatter formatter = new Formatter(sb, Locale.US);
-            formatter.format(format, "ID", "Created", "Name");
-            for (Study study : studyList) {
+            if (CollectionUtils.isNotEmpty(studyList)) {
+                StringBuilder sb = new StringBuilder();
+                String format = "%1$-12s %2$-20s %3$-40s%n";
+                Formatter formatter = new Formatter(sb, Locale.US);
+                formatter.format(format, "ID", "Created", "Name");
+                for (Study study : studyList) {
 
-                Date created = study.getCreated();
-                String formattedCreated = "";
-                if (created != null) {
-                    formattedCreated = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(
-                            created);
+                    Date created = study.getCreated();
+                    String formattedCreated = "";
+                    if (created != null) {
+                        formattedCreated = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT)
+                                .format(created);
+                    }
+
+                    formatter.format(format, study.getId(), formattedCreated, study.getName());
+                    formatter.flush();
                 }
-
-                formatter.format(format, study.getId(), formattedCreated, study.getName());
-                formatter.flush();
+                System.out.println(formatter.toString());
+                formatter.close();
             }
-            System.out.println(formatter.toString());
-            formatter.close();
+        } catch (MaPSeqDAOException e) {
+            e.printStackTrace();
         }
         return null;
     }
