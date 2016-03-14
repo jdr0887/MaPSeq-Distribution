5a95a6eb82326fa6662c2df23972b899df95ad7c
Jason Reilly <jdr0887@gmail.com>
2016-03-14 11:03:28 -0400

    adding logger

--- mapseq-commands/mapseq-commands-flowcell/src/main/java/edu/unc/mapseq/commands/flowcell/ListFlowcellFilesAction.java
+++ mapseq-commands/mapseq-commands-flowcell/src/main/java/edu/unc/mapseq/commands/flowcell/ListFlowcellFilesAction.java
@@ -12,6 +12,8 @@ import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.unc.mapseq.dao.FlowcellDAO;
 import edu.unc.mapseq.dao.MaPSeqDAOException;
@@ -21,6 +23,8 @@ import edu.unc.mapseq.dao.model.Flowcell;
 @Command(scope = "mapseq", name = "list-flowcell-files", description = "List Flowcell Files")
 @Service
 public class ListFlowcellFilesAction implements Action {
+
+    private static final Logger logger = LoggerFactory.getLogger(ListFlowcellFilesAction.class);
 
     @Reference
     private FlowcellDAO flowcellDAO;
@@ -34,8 +38,9 @@ public class ListFlowcellFilesAction implements Action {
 
     @Override
     public Object execute() {
-
-        try {
+        logger.debug("ENTERING execute()");
+        StringBuilder sb = new StringBuilder();
+        try (Formatter formatter = new Formatter(sb, Locale.US)) {
             Flowcell entity = flowcellDAO.findById(flowcellId);
             if (entity == null) {
                 System.out.println("Flowcell was not found");
@@ -43,8 +48,6 @@ public class ListFlowcellFilesAction implements Action {
             }
 
             String format = "%1$-12s %2$-20s %3$-24s %4$-80s %5$s%n";
-            StringBuilder sb = new StringBuilder();
-            Formatter formatter = new Formatter(sb, Locale.US);
             formatter.format(format, "ID", "Created", "MimeType", "Name", "Path");
 
             Set<FileData> fileDataSet = entity.getFileDatas();
@@ -64,7 +67,6 @@ public class ListFlowcellFilesAction implements Action {
                 }
             }
             System.out.println(formatter.toString());
-            formatter.close();
         } catch (MaPSeqDAOException e) {
             e.printStackTrace();
         }
