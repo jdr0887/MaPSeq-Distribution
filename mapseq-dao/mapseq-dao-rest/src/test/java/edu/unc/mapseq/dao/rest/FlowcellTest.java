ad17443322adeac1d76ae0edea95e811cfb3cbbf
Jason Reilly <jdr0887@gmail.com>
2016-04-01 11:32:37 -0400

    adding findByStudyName

--- mapseq-dao/mapseq-dao-rest/src/test/java/edu/unc/mapseq/dao/rest/FlowcellTest.java
+++ mapseq-dao/mapseq-dao-rest/src/test/java/edu/unc/mapseq/dao/rest/FlowcellTest.java
@@ -2,11 +2,20 @@ package edu.unc.mapseq.dao.rest;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
@@ -37,6 +46,27 @@ public class FlowcellTest {
             e.printStackTrace();
         }
 
+    }
+
+    @Test
+    public void findByStudyName() {
+        try {
+            RESTDAOManager daoMgr = RESTDAOManager.getInstance();
+            List<Flowcell> entityList = daoMgr.getMaPSeqDAOBeanService().getFlowcellDAO().findByStudyName("NC_GENES");
+            if (CollectionUtils.isNotEmpty(entityList)) {
+                for (Flowcell flowcell : entityList) {
+                    JAXBContext context = JAXBContext.newInstance(Flowcell.class);
+                    Marshaller m = context.createMarshaller();
+                    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+                    File moduleClassXMLFile = new File("/tmp/flowcells",
+                            String.format("%s-%d.xml", "Flowcell", flowcell.getId()));
+                    FileWriter fw = new FileWriter(moduleClassXMLFile);
+                    m.marshal(flowcell, fw);
+                }
+            }
+        } catch (MaPSeqDAOException | JAXBException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -84,9 +114,8 @@ public class FlowcellTest {
         RESTDAOManager daoMgr = RESTDAOManager.getInstance();
         FlowcellDAO flowcellDAO = daoMgr.getMaPSeqDAOBeanService().getFlowcellDAO();
         try {
-            Flowcell entity = new Flowcell();
+            Flowcell entity = new Flowcell("test");
             entity.setBaseDirectory("adsf");
-            entity.setName("test");
             Long id = flowcellDAO.save(entity);
             System.out.println(id);
         } catch (MaPSeqDAOException e) {
