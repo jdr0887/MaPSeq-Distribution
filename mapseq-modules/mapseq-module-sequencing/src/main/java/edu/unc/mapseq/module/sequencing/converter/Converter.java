d069613876a39e158d08936f30c172371e895a61
Jason Reilly <jdr0887@gmail.com>
2016-05-13 15:19:32 -0400

    externalizing

--- mapseq-modules/mapseq-module-sequencing/src/main/java/edu/unc/mapseq/module/sequencing/converter/Converter.java
+++ mapseq-modules/mapseq-module-sequencing/src/main/java/edu/unc/mapseq/module/sequencing/converter/Converter.java
@@ -1,15 +1,10 @@
 package edu.unc.mapseq.module.sequencing.converter;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
-import edu.unc.mapseq.module.DefaultModuleOutput;
 import edu.unc.mapseq.module.Module;
-import edu.unc.mapseq.module.ModuleException;
-import edu.unc.mapseq.module.ModuleOutput;
 import edu.unc.mapseq.module.annotations.Application;
 import edu.unc.mapseq.module.annotations.InputArgument;
 import edu.unc.mapseq.module.annotations.InputValidations;
@@ -21,36 +16,36 @@ import edu.unc.mapseq.module.constraints.FileIsReadable;
  * 
  * @author jdr0887
  */
-@Application(name = "Converter")
+@Application(name = "Converter", executable = "$JAVA7_HOME/bin/java -Xmx4g -jar $%s_SEQUENCING_TOOLS/converter.jar")
 public class Converter extends Module {
 
     @NotNull(message = "bamFile is required", groups = InputValidations.class)
     @FileIsNotEmpty(message = "bamFile is empty", groups = InputValidations.class)
     @FileIsReadable(message = "bamFile is not readable", groups = InputValidations.class)
-    @InputArgument
+    @InputArgument(flag = "-b")
     private File bamFile;
 
     @NotNull(message = "vcfFile is required", groups = InputValidations.class)
     @FileIsNotEmpty(message = "vcfFile is empty", groups = InputValidations.class)
     @FileIsReadable(message = "vcfFile is not readable", groups = InputValidations.class)
-    @InputArgument
+    @InputArgument(flag = "-v")
     private File vcfFile;
 
     @NotNull(message = "outputDir is required", groups = InputValidations.class)
     @FileIsNotEmpty(message = "invalid output file", groups = OutputValidations.class)
-    @InputArgument
+    @InputArgument(flag = "-o")
     private File outputDir;
 
     @NotNull(message = "convertGenome is required", groups = InputValidations.class)
-    @InputArgument
+    @InputArgument(flag = "-g")
     private Boolean convertGenome = Boolean.FALSE;
 
     @NotNull(message = "convertExome is required", groups = InputValidations.class)
-    @InputArgument
+    @InputArgument(flag = "-x")
     private Boolean convertExome = Boolean.FALSE;
 
     @NotNull(message = "generateMetrics is required", groups = InputValidations.class)
-    @InputArgument
+    @InputArgument(flag = "-m")
     private Boolean generateMetrics = Boolean.FALSE;
 
     public Converter() {
@@ -62,43 +57,9 @@ public class Converter extends Module {
         return Converter.class;
     }
 
-    @SuppressWarnings("static-access")
     @Override
-    public ModuleOutput call() throws ModuleException {
-        DefaultModuleOutput moduleOutput = new DefaultModuleOutput();
-        int exitCode = 0;
-        try {
-
-            List<String> args = new ArrayList<String>();
-
-            args.add("-v " + vcfFile.getAbsolutePath());
-            args.add("-b " + bamFile.getAbsolutePath());
-            args.add("-o " + outputDir.getAbsolutePath());
-
-            if (convertGenome) {
-                args.add("-g");
-            }
-
-            if (convertExome) {
-                args.add("-x");
-            }
-
-            if (generateMetrics) {
-                args.add("-m");
-            }
-
-            org.renci.sequencing.converter.Converter converter = new org.renci.sequencing.converter.Converter();
-            converter.main(args.toArray(new String[args.size()]));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            moduleOutput.setExitCode(-1);
-            moduleOutput.setError(new StringBuilder(e.getMessage()));
-            return moduleOutput;
-        }
-        moduleOutput.setExitCode(exitCode);
-
-        return moduleOutput;
+    public String getExecutable() {
+        return String.format(getModuleClass().getAnnotation(Application.class).executable(), getWorkflowName().toUpperCase());
     }
 
     public File getBamFile() {
@@ -147,6 +108,26 @@ public class Converter extends Module {
 
     public void setGenerateMetrics(Boolean generateMetrics) {
         this.generateMetrics = generateMetrics;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Converter [bamFile=%s, vcfFile=%s, outputDir=%s, convertGenome=%s, convertExome=%s, generateMetrics=%s]",
+                bamFile, vcfFile, outputDir, convertGenome, convertExome, generateMetrics);
+    }
+
+    public static void main(String[] args) {
+        Converter module = new Converter();
+        module.setWorkflowName("TEST");
+        module.setBamFile(new File("/tmp", "input.sam"));
+        module.setOutputDir(new File("/tmp"));
+        module.setVcfFile(new File("/tmp", "some.vcf"));
+        module.setConvertExome(Boolean.TRUE);
+        try {
+            module.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
