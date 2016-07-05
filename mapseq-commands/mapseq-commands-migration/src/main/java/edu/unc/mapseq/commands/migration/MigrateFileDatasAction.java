ff99fdeafeeaf6bc8ded729861131639b40c0755
Jason Reilly <jdr0887@gmail.com>
2016-07-05 14:33:14 -0400

    misc

--- mapseq-commands/mapseq-commands-migration/src/main/java/edu/unc/mapseq/commands/migration/MigrateFileDatasAction.java
+++ mapseq-commands/mapseq-commands-migration/src/main/java/edu/unc/mapseq/commands/migration/MigrateFileDatasAction.java
@@ -1,16 +1,22 @@
 package edu.unc.mapseq.commands.migration;
 
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
-import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.unc.mapseq.dao.FileDataDAO;
 import edu.unc.mapseq.dao.MaPSeqDAOBeanService;
+import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.StudyDAO;
+import edu.unc.mapseq.dao.model.FileData;
+import edu.unc.mapseq.dao.model.Study;
 
 @Command(scope = "mapseq", name = "migrate-file-datas", description = "Migrate FileData instances")
 @Service
@@ -21,23 +27,51 @@ public class MigrateFileDatasAction implements Action {
     @Reference
     private MaPSeqDAOBeanService maPSeqDAOBeanService;
 
-    @Option(name = "--dryRun", description = "Don't move anything", required = false, multiValued = false)
-    private Boolean dryRun = Boolean.FALSE;
-
     @Override
     public Object execute() {
-        logger.debug("ENTERING doExecute()");
-        MigrateFileDatasRunnable runnable = new MigrateFileDatasRunnable(dryRun, maPSeqDAOBeanService);
-        Executors.newSingleThreadExecutor().execute(runnable);
+        logger.debug("ENTERING execute()");
+        StudyDAO studyDAO = maPSeqDAOBeanService.getStudyDAO();
+
+        FileDataDAO fileDataDAO = maPSeqDAOBeanService.getFileDataDAO();
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        try {
+            List<FileData> fileDataList = fileDataDAO.findAll();
+
+            List<Study> studyList = studyDAO.findAll();
+
+            for (FileData fileData : fileDataList) {
+                es.submit(() -> {
+                    for (Study study : studyList) {
+
+                        if (!fileData.getPath().startsWith(String.format("/projects/sequence_analysis/medgenwork/%s", study.getName()))) {
+                            continue;
+                        }
+
+                        if (fileData.getPath().contains("NCGenesVCFCompare")) {
+                            continue;
+                        }
+
+                        try {
+                            logger.info(fileData.toString());
+                            String path = fileData.getPath().replace(
+                                    String.format("/projects/sequence_analysis/medgenwork/%s", study.getName()),
+                                    String.format("/projects/sequence_analysis/medgenwork/prod/%s", study.getName()));
+                            fileData.setPath(path);
+                            fileDataDAO.save(fileData);
+                            logger.info(fileData.toString());
+                        } catch (MaPSeqDAOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+            es.shutdown();
+        } catch (MaPSeqDAOException e) {
+            e.printStackTrace();
+        }
+
         return null;
-    }
-
-    public Boolean getDryRun() {
-        return dryRun;
-    }
-
-    public void setDryRun(Boolean dryRun) {
-        this.dryRun = dryRun;
     }
 
 }
