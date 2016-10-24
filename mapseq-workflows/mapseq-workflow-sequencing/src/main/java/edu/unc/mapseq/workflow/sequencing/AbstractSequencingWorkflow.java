7f3f80fea4fe1908c81ed63971f15ea05118870b
Jason Reilly <jdr0887@gmail.com>
2016-10-24 14:34:38 -0400

    using earlier collections version

--- mapseq-workflows/mapseq-workflow-sequencing/src/main/java/edu/unc/mapseq/workflow/sequencing/AbstractSequencingWorkflow.java
+++ mapseq-workflows/mapseq-workflow-sequencing/src/main/java/edu/unc/mapseq/workflow/sequencing/AbstractSequencingWorkflow.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
